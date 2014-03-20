9f617cb782a039ce64ac0a97bd41763a793ac0cd
Al Wold <alwold@gmail.com>
2014-03-19 17:13:12 -0700

    use printf

--- farkle.s
+++ farkle.s
@@ -1,17 +1,17 @@
+.extern printf
 .data
 
 message:
-  .ascii "Hello World\n\0"
+	.asciz "Hello World\n"
 
-.globl _start
-_start:
-# print shit
-  movl $4, %eax # write syscall
-  movl $1, %ebx # stdout?
-  movl $message, %ecx
-  movl $13, %edx
-  int $0x80
+.text
+.global main
+main:
+	# print
+	movl $message, %edi # argument
+	movl $0, %eax # number of args?
+	call printf
 
-# call exit syscall
-  mov $1, %eax
-  int $0x80
+	# call exit syscall
+	mov $1, %eax
+	int $0x80
